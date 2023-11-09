da5dac37b444b092622ba3c01ac1527e772e6d66
AbrarAhamed18 <abrar18ahamedz@gmail.com>
2023-11-09 11:59:07 +0530

    "home page"

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,18 +1,46 @@
-import React from 'react'
+import React from 'react';
+import ReactStars from "react-rating-stars-component";
+import { Link } from 'react-router-dom';
 
 const ProductCard = () => {
+    
   return (
     <div className='col-3'>
-        <div className="product-card">
+        <Link className="product-card position-relative">
+            <div className="wishlist-icon position-absolute">
+                <Link><img src="images/wish.svg" alt="wishlist" /></Link>
+            </div>
             <div className="product-image">
-                <img src="images/watch.jpg" alt="product image" />
+                <img src="images/watch.jpg" className='img-fluid' alt="product image" />
+                {/* <img src="images/speaker.jpg" alt="product image" /> */}
             </div>
             <div className="product-details">
                 <h6 className="brand">FireBoltt</h6>
                 <h5 className="poduct-title">V3 smartwatch cellular with gps</h5>
+                <ReactStars
+                    count={5}
+                    size={24}
+                    value={4}
+                    edit={false}
+                    activeColor="#ffd700"
+                    />
                 <p className="price">$100.00</p>
             </div>
-        </div>
+            <div className="action-bar position-absolute">
+                <div className='d-flex flex-column gap-15'>
+                <Link>
+                        <img src="images/prodcompare.svg" alt="add cart" />
+                    </Link>
+                    <Link>
+                        <img src="images/view.svg" alt="view" />
+                    </Link>
+                    <Link>
+                        <img src="images/add-cart.svg" alt="add cart" />
+                    </Link>
+
+                </div>
+            </div>
+        </Link>
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import Marquee from "react-fast-marquee";
 import BlogCard from '../components/BlogCard';
 import ProductCard from '../components/ProductCard';
+import SpecialProduct from '../components/SpecialProduct';
 
 const Home = () => {
   return (
@@ -125,7 +126,7 @@ const Home = () => {
         </div>
     </section>
 
-    <section className='home-wrapper-3 py-5'>
+    <section className='home-wrapper-2 py-5'>
         <div className='container-xxl'>
             <div className='row'>
                 <div className='col-12'>
@@ -193,6 +194,54 @@ const Home = () => {
         </div>
     </section>
 
+    <section className="featured-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+            <div className="row">
+                <div className="col-12">
+                   <h3 className="section-heading">Featured Collection</h3> 
+                </div>
+                <ProductCard/>
+                <ProductCard/>
+                <ProductCard/>
+                <ProductCard/>
+            </div>
+        </div>
+    </section>
+
+    <section className='special-wrapper py-5 home-wrapper-2'>
+        <div className="container-xxl">
+            <div className="row">
+                <div className="col-12">
+                    <h3 className='section-heading'>Special Products</h3>
+                </div>
+            </div>
+            <div className="row">
+                <SpecialProduct/>
+                <SpecialProduct/>
+                <SpecialProduct/>
+            </div>
+        </div>
+
+    </section>
+
+    <section className="popular-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+            <div className="row">
+                <div className="col-12">
+                   <h3 className="section-heading">Our Popular Products </h3> 
+                </div>
+                
+            </div>
+            <div className="row">
+                
+                <ProductCard/>
+                <ProductCard/>
+                <ProductCard/>
+                <ProductCard/>
+            </div>
+        </div>
+    </section>
+
     <section className='marquee-wrapper py-5'>
         <div className='container-xxl'>
             <div className='row'>
@@ -230,19 +279,7 @@ const Home = () => {
         </div>
     </section>
 
-    <section className="blog-wrapper py-5 home-wrapper-2">
-        <div className="container-xxl">
-            <div className="row">
-                <div className="col-12">
-                   <h3 className="section-heading">Featured Collection</h3> 
-                </div>
-                <ProductCard/>
-                <ProductCard/>
-                <ProductCard/>
-                <ProductCard/>
-            </div>
-        </div>
-    </section>
+    
 
     <section className="blog-wrapper py-5 home-wrapper-2">
         <div className="container-xxl">
